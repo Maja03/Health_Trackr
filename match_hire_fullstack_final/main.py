70ad8c058b42883fdc523d53dda5c405ffa5c5f3
Maja Czekała <112630417+Maja03@users.noreply.github.com>
2025-07-14 08:22:41 +0200

    Delete match_hire_fullstack_final directory

--- match_hire_fullstack_final/main.py
+++
@@ -1,6 +0,0 @@
-# Import the FastAPI app from the simple_advanced_api module
-from simple_advanced_api import app
-
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
